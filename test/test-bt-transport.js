27038a67ce768f2f892614e7ac30c44e32704423
Juan Gomez <atilag@gmail.com>
2016-07-20 18:04:41 +0200

    Moving forward with the Bluetooth transport layer... but, there's no Bluetooth server side libraries. So, I'd probably stop here until figure out what to do... maybe build my own.

---
+++ test/test-bt-transport.js
@@ -0,0 +1,31 @@
+// Test
+
+var assert = require('chai').assert;
+var FlyWeb = require('../lib/flyweb');
+var FlyWebPublishOptions = require('../lib/flywebpublishoptions');
+
+var fw = FlyWeb();
+
+fw.on('websocket', function(event){
+	console.log('test.js: webscoket!');
+	event.accept().then( ws => {
+		ws.on('message', data => {
+			console.log('test.js: Date received via websocket: ' + data);
+			try{ 
+				console.log("test.js: Sending back data to the client");
+				ws.send(data);
+			}catch(ex){
+				console.log(ex);
+			}
+		});
+	});
+});
+
+var fwOptions = FlyWebPublishOptions();
+fwOptions.uiUri = 'client/index.html';
+fwOptions.transport = FlyWebPublishOptions.BLUETOOTH;
+fw.publishServer('FlyWebTestBT', fwOptions).then(server => {
+	console.log("Server published");
+}).catch(ex => {
+	console.error(ex);
+});
